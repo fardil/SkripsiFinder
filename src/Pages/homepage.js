00a49b2c75ff4131ced45d7ff66a1fb06dd040f9
rividya <ivy.aluna@gmail.com>
2020-05-29 16:02:04 +0700

    edit layout

--- src/Pages/homepage.js
+++ src/Pages/homepage.js
@@ -10,7 +10,7 @@ return (
     <body>
       {/* Navbar */}
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Skripsi Finder</a>
+        <a className="navbar-brand" href="homepage">Skripsi Finder</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
