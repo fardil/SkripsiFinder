c64d57e677f9e2f0a3d1421d0369d638d6592bc3
rividya <ivy.aluna@gmail.com>
2020-05-29 16:04:30 +0700

    edit layout

--- src/Pages/skripsi.js
+++ src/Pages/skripsi.js
@@ -17,7 +17,7 @@ return (
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
+                <li className="nav-item">
                     <a className="nav-link" href="homepage">Home</a>
                 </li>
                 <li className="nav-item">
