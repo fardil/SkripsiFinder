6ecfd8b8372a47a5de83eddf239416f7fb30687d
fardil <faradillaazra@gmail.com>
2020-06-08 14:26:08 +0700

    navbar and footer responsive

--- src/Pages/homepage.js
+++ src/Pages/homepage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
 import axios from 'axios';
 import qs from 'qs';
+import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 export default function Homepage(){
   
@@ -137,23 +137,15 @@ const content = value.skripsis.map((skripsi) =>
 return (
     <body>
       {/* Navbar */}
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">Skripsi Finder</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="/">Home</a>
-                </li>
-                <li className="nav-item">
-                    {/* <a className="nav-link" href="Login">Login</a> */}
-                </li>
-            </ul>
-        </div>
-      </nav>
+      <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">Skripsi Finder</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
 
       <div className="header">
         <h1>
